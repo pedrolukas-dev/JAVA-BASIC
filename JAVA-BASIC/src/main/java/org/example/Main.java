d84734945f8a96e564b7a682d376999d1eb6c3e8
pedrolukas-dev <pedro.l.santos14@ba.estudante.senai.br>
2026-08-11 21:27:11 -0300

    Add files via upload

---
+++ JAVA-BASIC/src/main/java/org/example/Main.java
@@ -0,0 +1,22 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    static void main() {
+        // declaração de variavel
+        String nome = "Marta";
+        int idade =30;
+        double peso = 65.2;
+
+        // Declaração de constantes
+        final String CPF = "123.456.789-00";
+     //exibindo resultado
+     //contatenando  com sinal de "+"
+        System.out.println("O nome do usuário é:" + nome);
+        System.out.println("O CPF do usuário é:" + CPF);
+        System.out.println("A idade do usuário é:" + idade +"anos");
+        System.out.println("O peso do usuário é:" + peso + "quilos" );
+    }
+}
+
